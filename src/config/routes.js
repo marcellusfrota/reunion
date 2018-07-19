05c91783825d6e44e6194779cd7199cef4b96ad2
jhudsonsg <jhudsonsg@gmail.com>
2018-07-19 09:35:05 -0300

    New route configuration

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,22 @@
-import Index from '../index/Index';
+import Index from '../components/Index';
+import Login from '../components/Login';
+import Register from '../components/Register';
 
 export const routes = [
     {
         path: '/',
         name: 'index',
         component: Index
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: Register
     },
     {
         path: '*',
